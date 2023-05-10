10cadb92e92f59b4761cbb825ec1800eb4f539f5
Hemant Kumar <Hemant Kumar@LAPTOP-5DOG4BF4>
2023-05-10 21:30:40 +0530

    Add project files.

---
+++ TestApp2/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TestApp2.Models;
+
+namespace TestApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+      /*  public string ReturnOrder()
+        {
+            string name = "My name is Hemant Kumar";
+            string newname = name.Split(' ');
+            foreach (var abc in newname)
+            {
+                
+            }
+            return "";
+        }*/
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
